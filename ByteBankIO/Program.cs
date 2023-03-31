5ca3dee30d3f7f14c1cd312b706320edd56c6253
Luciano Bueno <luciano.bueno@ingacode.com.br>
2023-03-31 17:42:28 -0300

    CLasse FILE para leitura e gravação

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,7 +1,27 @@
 ﻿internal partial class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
-        UsandoStreamDaConsole();
+        //Console.WriteLine("Digite seu nome:");
+        //var nome = Console.ReadLine();
+
+        var linhas = File.ReadAllLines("contas.csv");
+        Console.WriteLine(linhas.Length);
+
+        /*
+        foreach (var linha in linhas)
+        {
+            Console.WriteLine(linha);
+        }
+        */
+
+        var bytesArquivo = File.ReadAllBytes("contas.csv");
+        Console.WriteLine($"Arquivo contas.txt possui {bytesArquivo.Length} bytes");
+
+        File.WriteAllText("escrevendoComClasseFile.txt", "Testando File.WriteAllText");
+
+        Console.WriteLine("Aplicação Finalizada ...");
+
+        Console.ReadLine();
     }
 }
